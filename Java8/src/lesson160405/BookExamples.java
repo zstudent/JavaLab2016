b7d5c12c22eb9c045cb696f4e7172e3297855734
Zaal <Zaal_Lyanov@epam.com>
2016-04-07 16:43:22 +0300

    lesson 160407 part 1

--- Java8/src/lesson160405/BookExamples.java
+++ Java8/src/lesson160405/BookExamples.java
@@ -1,89 +1,31 @@
 package lesson160405;
 
-import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 import java.util.stream.Stream;
+
+import library.Book;
+import library.Book.Topic;
 
 public class BookExamples {
 
-	enum Topic {
-		PROGRAMMING, FICTION, SHORT
-	}
-
-	static class Book {
-
-		String title;
-		int[] pageCounts;
-		double height;
-		Topic topic;
-
-		public Topic getTopic() {
-			return topic;
-		}
-
-		public void setTopic(Topic topic) {
-			this.topic = topic;
-		}
-
-		public Book(String title, int[] pageCounts, double height, Topic topic) {
-			super();
-			this.title = title;
-			this.pageCounts = pageCounts;
-			this.height = height;
-		}
-
-		public String getTitle() {
-			return title;
-		}
-
-		public void setTitle(String title) {
-			this.title = title;
-		}
-
-		public int[] getPageCounts() {
-			return pageCounts;
-		}
-
-		public void setPageCounts(int[] pageCounts) {
-			this.pageCounts = pageCounts;
-		}
-
-		public double getHeight() {
-			return height;
-		}
-
-		public void setHeight(double height) {
-			this.height = height;
-		}
-
-	}
 
 	public static void main(String[] args) {
 
-		Book b1 = new Book("Sherlock Holmes", new int[] { 256 }, 25.5,
-				Topic.SHORT);
-		Book b2 = new Book("Tom Sawyer", new int[] { 300 }, 20.5, Topic.FICTION);
-		Book b3 = new Book("War and Peace", new int[] { 500, 450, 300, 600 },
-				50.0, Topic.FICTION);
-		Book b4 = new Book("Java8 basics", new int[] { 100 }, 15.0,
-				Topic.PROGRAMMING);
 
-		List<Book> library = Arrays.asList(b1, b2, b3, b4);
-		
-		Stream<Book> filteredStream = library.stream().filter(b -> b.getTopic() == Topic.PROGRAMMING);
-		
-		Stream<String> map = library.stream().map(b -> b.getTitle());
-		
-		library.stream().sorted(Comparator.comparing(Book::getTitle));
+		Stream<Book> filteredStream = Book.library.stream().filter(
+				b -> b.getTopic() == Topic.PROGRAMMING);
 
-		library.stream().sorted(new Comparator<Book>() {
+		Stream<String> map = Book.library.stream().map(b -> b.getTitle());
+
+		Book.library.stream().sorted(Comparator.comparing(Book::getTitle));
+
+		Book.library.stream().sorted(new Comparator<Book>() {
 			@Override
 			public int compare(Book o1, Book o2) {
 				return o1.getTitle().compareTo(o2.getTitle());
 			}
 		});
-		
+
 	}
 
 }
---
+++ Java8/src/library/Book.java
@@ -0,0 +1,78 @@
+package library;
+
+import java.time.Year;
+import java.util.Arrays;
+import java.util.List;
+
+public class Book {
+
+	public enum Topic {
+		PROGRAMMING, FICTION, SHORT, HUMOR
+	}
+	
+	public static List<Book> library;
+	
+
+	static void createLibrary() {
+		Book b1 = new Book("Sherlock Holmes", new int[] { 256 }, 25.5,
+				Topic.SHORT, Arrays.asList("Arthur Conan Doyle"), Year.of(1920));
+		Book b2 = new Book("Tom Sawyer", new int[] { 300 }, 20.5, Topic.FICTION, Arrays.asList("Mark Twain"), Year.of(1930));
+		Book b3 = new Book("War and Peace", new int[] { 500, 450, 300, 600 },
+				50.0, Topic.FICTION, Arrays.asList("Lev Tolstoy"), Year.of(1900));
+		Book b4 = new Book("Java8 basics", new int[] { 100 }, 15.0,
+				Topic.PROGRAMMING, Arrays.asList("Smith","Johnes","Peters"), Year.of(2014));
+		Book b5 = new Book("12 chairs", new int[] { 230 }, 18.0,
+				Topic.HUMOR, Arrays.asList("Ilf","Petrov"), Year.of(2014));
+		
+		library = Arrays.asList(b1, b2, b3, b4, b5);
+	}
+	
+	static {
+		createLibrary();
+	}
+	
+
+	
+	String title;
+	int[] pageCounts;
+	double height;
+	Book.Topic topic;
+	List<String> authors;
+	Year year;
+
+	public Book(String title, int[] pageCounts, double height, Book.Topic topic,
+			List<String> authors, Year year) {
+		super();
+		this.title = title;
+		this.pageCounts = pageCounts;
+		this.height = height;
+		this.topic = topic;
+		this.authors = authors;
+		this.year = year;
+	}
+
+	public List<String> getAuthors() {
+		return authors;
+	}
+
+	public Year getYear() {
+		return year;
+	}
+
+	public Book.Topic getTopic() {
+		return topic;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public int[] getPageCounts() {
+		return pageCounts;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+}
